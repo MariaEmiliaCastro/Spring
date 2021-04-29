da35d582652c2516718a9b7383a8883636cff4ef
MariaEmiliaCastro <emyly.mary@gmail.com>
2021-04-29 12:19:31 -0300

    Atualizando aprendizado

---
+++ 03-actuator-demo/src/main/java/com/luv2code/initializr/MyInitApp/MyInitAppApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.initializr.MyInitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInitAppApplication.class, args);
+	}
+
+}
---
+++ 03-actuator-demo/src/test/java/com/luv2code/initializr/MyInitApp/MyInitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.initializr.MyInitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
